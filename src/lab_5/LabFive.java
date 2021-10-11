e266c5e80889074f00ce312abd12178ea120e7f5
dimaTsaruk <dima.beseder@gmail.com>
2021-10-11 00:18:59 -0500

    csc

--- src/lab_5/LabFive.java
+++ src/lab_5/LabFive.java
@@ -2,6 +2,11 @@ package lab_5;
 
 public class LabFive {
     public static void main(String[] args) {
-        // lab # 5 ...
+
+        byte b1 = 127;
+        b1 += 1;
+        b1 = (byte) (b1 + 1);
+
+        System.out.println(b1);
     }
 }
