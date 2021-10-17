30f99ed6a70bfb280f6a5ccbb6de1eaa162d891f
dimaTsaruk <dima.beseder@gmail.com>
2021-10-17 12:18:07 -0500

    tutorial updated with Arrays

---
+++ src/tutorials_java/Employee.java
@@ -0,0 +1,33 @@
+package tutorials_java;
+
+public class Employee {
+    private int empNum;
+    private double empSal;
+
+    Employee(int e, double s) {
+        empNum = e;
+        empSal = s;
+    }
+
+    public int getEmpNum() {
+        return empNum;
+    }
+
+    public double getEmpSal() {
+        return empSal;
+    }
+
+    public static void main(String[] args) {
+        Employee[] emps = new Employee[7];
+        final int START_NUM = 101;
+        final double STARTING_SALARY = 15_000;
+        for (int x = 0; x < emps.length; x++)
+            emps[x] = new Employee(START_NUM + x, STARTING_SALARY);
+
+        for (int x = 0; x < emps.length; ++x)
+            System.out.println(emps[x].getEmpNum() + " " +
+                    emps[x].getEmpSal());
+        for (Employee workers : emps)
+            System.out.println(workers.getEmpNum() + " " + workers.getEmpSal());
+    }
+}
---
+++ src/tutorials_java/demo.java
@@ -0,0 +1,33 @@
+package tutorials_java;
+
+public class demo {
+    public static void main(String[] args) {
+
+        // Returning an array from a method
+        /*public static int[] getArray () {
+            int[] scores = {90, 80, 70, 60};
+            return scores;
+        }
+        // you can store its returned value in any integer array reference.
+        int[] scoresFromMethod = getArray();//
+        */
+
+        // A for loop with an early exit
+        /*for (int x = 0; x < NUMBER_OF_ITEMS; ++x) {
+            if (itemOrdered == validValues[x]) {
+                isValidItem = true;
+                itemPrice = prices[x];
+                x = NUMBER_OF_ITEMS
+            }} */
+
+        // A for loop that uses a compound test for termination
+        /*
+        for(int x = 0; x < NUMBER_OF_ITEMS && !isValidItem; ++x) {
+                if(itemOrdered == validValues[x]) {
+                isValidItem = true;
+                itemPrice = prices [x];
+                          }
+                        } */
+
+    }
+}
